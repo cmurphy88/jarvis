15db09b917c001d432a359913d364fc3b97b8308
cmurphy88 <cmurphy715@qub.ac.uk>
2022-06-24 09:26:04 +0100

    dummy data added to the db

--- migrations/local-data-seed/V2.0__mock_data.sql
+++ migrations/local-data-seed/V2.0__mock_data.sql
@@ -40,6 +40,7 @@ INSERT INTO routine (room_id, user_id, start_time, end_time)
 
 -- media --
 INSERT INTO media (ip_address) VALUES ('180:888:999');
+INSERT INTO media (ip_address) VALUES ('167:344:900');
 
 -- media routine setting --
 INSERT INTO media_routine_setting (media_id, routine_id, media_url, is_active, is_playing)
@@ -49,6 +50,7 @@ INSERT INTO media_routine_setting (media_id, routine_id, media_url, is_active, i
 
 -- trv --
 INSERT INTO trv (ip_address) VALUES ('213:456:686');
+INSERT INTO trv (ip_address) VALUES ('456:112:998');
 
 -- trv routine settings --
 INSERT INTO trv_routine_setting (trv_id, routine_id, temperature, is_active)
@@ -58,8 +60,9 @@ INSERT INTO trv_routine_setting (trv_id, routine_id, temperature, is_active)
 
 -- light --
 INSERT INTO light (ip_address) VALUES ('123:678:987');
+INSERT INTO light (ip_address) VALUES ('321:678:789');
 
--- ligjt routine settings --
+-- light routine settings --
 INSERT INTO light_routine_setting (light_id, routine_id, brightness, is_active)
     VALUES (1, 1, 50, TRUE);
 INSERT INTO light_routine_setting (light_id, routine_id, brightness, is_active)
@@ -76,6 +79,8 @@ INSERT INTO routine_time_entries (routine_id, time_entry)
 -- routine alert --
 INSERT INTO routine_alert (message, routine_id)
     VALUES ('unrecognised face in the living room', 1);
+INSERT INTO routine_alert (message, routine_id)
+    VALUES ('unrecognised face in the kitchen, would you like to add them to the house?', 2);
 
 
 
