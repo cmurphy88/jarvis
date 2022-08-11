7ec4e7715bf0ab48c4fc75036c45c47522271f2c
cmurphy88 <cmurphy715@qub.ac.uk>
2022-08-11 15:35:31 +0100

    aws api

--- api/db.py
+++ api/db.py
@@ -1,3 +1,5 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
@@ -11,7 +13,12 @@ DATABASE_NAME = config.DATABASE_NAME
 
 SQLALCHEMY_DATABASE_URL = f"postgresql://{DATABASE_USERNAME}:{DATABASE_PASSWORD}@{DATABASE_HOST}/{DATABASE_NAME}"
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+def get_database_url():
+    return os.environ.get('DATABASE_URL', SQLALCHEMY_DATABASE_URL)
+
+
+engine = create_engine(get_database_url())
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
