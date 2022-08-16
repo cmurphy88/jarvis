246ce417e127b7b8d09a00439ec0e50e3aef34fa
cmurphy88 <cmurphy715@qub.ac.uk>
2022-08-16 16:00:36 +0100

    allow all origins

--- api/app/main.py
+++ api/app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from .home import router as home_router
 from .room import router as room_router
 from .routine import router as routine_router
@@ -9,39 +10,26 @@ from .light import router as light_router
 from .auth import router as auth_router
 from .health import router as health_router
 from .users import router as user_router
-from starlette.middleware import Middleware
-from starlette.middleware.cors import CORSMiddleware
-
-middleware = [
-    Middleware(
-        CORSMiddleware,
-        allow_origins=['*'],
-        allow_credentials=True,
-        allow_methods=['*'],
-        allow_headers=['*']
-    )
-]
-
 
 app = FastAPI(title="JARVISWebApp",
-              version="0.0.2",
-              middleware=middleware)
+              version="0.0.2")
 
 origins = [
     "http://localhost.tiangolo.com",
     "https://localhost.tiangolo.com",
     "http://localhost",
     "http://localhost:3000",
-    "https://*",
+    "https://jarvis-qub.co.uk",
+    "https://www.jarvis-qub.co.uk"
 ]
 
-# app.add_middleware(
-#     CORSMiddleware,
-#     allow_origins=['*'],
-#     allow_credentials=True,
-#     allow_methods=["*"],
-#     allow_headers=["*"],
-# )
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(auth_router.router)
 app.include_router(user_router.router)
