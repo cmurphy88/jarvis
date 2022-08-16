db47f29921c1b33f346bead10fc856ac27980b2e
cmurphy88 <cmurphy715@qub.ac.uk>
2022-08-16 15:35:15 +0100

    allow all origins

--- api/app/main.py
+++ api/app/main.py
@@ -16,12 +16,16 @@ app = FastAPI(title="JARVISWebApp",
               version="0.0.2")
 
 origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:3000",
     "*",
 ]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=['*'],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
