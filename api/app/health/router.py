0d12c7f207811bc32a1d3ca6a82ba5a7e9aeb082
cmurphy88 <cmurphy715@qub.ac.uk>
2022-08-17 09:59:33 +0100

    health check endpoint fixed

--- api/app/health/router.py
+++ api/app/health/router.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, status
 router = APIRouter(tags=['Health'], prefix='/health')
 
 
-@router.post('/', status_code=status.HTTP_200_OK)
+@router.get('/', status_code=status.HTTP_200_OK)
 async def check_health():
 
     return
