e6b0d35bad38d15c6012aa622b6bc3fd1c8ba510
cmurphy88 <cmurphy715@qub.ac.uk>
2022-08-17 14:13:16 +0100

    testing aws env

--- ui/src/config/index.jsx
+++ ui/src/config/index.jsx
@@ -1,6 +1,9 @@
 function getApiUrl() {
-    // return process.env.REACT_APP_API_URL || "http://127.0.0.1:8000/";
-    return "https://jarvis-1.5a25j6q6mjvnu.eu-west-1.cs.amazonlightsail.com/"
+    const test = process.env.REACT_APP_API_URL || "http://127.0.0.1:8000/";
+    console.log("URL...", test)
+    return test;
+    // return "https://jarvis-1.5a25j6q6mjvnu.eu-west-1.cs.amazonlightsail.com/";
+
 }
 
 export {getApiUrl}; 
