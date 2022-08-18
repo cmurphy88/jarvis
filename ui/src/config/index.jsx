3744516d7876e4caa9228572ba5ac4f46b6254ac
cmurphy88 <cmurphy715@qub.ac.uk>
2022-08-18 09:18:13 +0100

    aws configuration

--- ui/src/config/index.jsx
+++ ui/src/config/index.jsx
@@ -1,9 +1,5 @@
 function getApiUrl() {
-    const test = process.env.REACT_APP_API_URL || "http://127.0.0.1:8000/";
-    console.log("URL...", test)
-    return test;
-    // return "https://jarvis-1.5a25j6q6mjvnu.eu-west-1.cs.amazonlightsail.com/";
-
+    return process.env.REACT_APP_API_URL || "http://127.0.0.1:8000/";
 }
 
 export {getApiUrl}; 
