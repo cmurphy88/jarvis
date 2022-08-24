b0b64ef039b21a83b41d78c712b25df067bcb302
cmurphy88 <cmurphy715@qub.ac.uk>
2022-08-24 19:38:07 +0100

    creating automation handler

--- pi/handler/jarvis_automation_handler.py
+++ pi/handler/jarvis_automation_handler.py
@@ -1,13 +1,15 @@
 import time
-
 import requests
 
 while True:
 
-    user_id = int(requests.get('http://localhost:8001/find-faces'))
+    user_id = requests.get('http://localhost:8001/find-faces')
+    print(user_id)
 
-    if user_id > 0:
-        routine = requests.get('https://jarvis-1.5a25j6q6mjvnu.eu-west-1.cs.amazonlightsail.com/')
+    time.sleep(10)
 
-    print(routine)
-    time.sleep(5)
+    # if user_id > 0:
+    #     routine = requests.get('https://jarvis-1.5a25j6q6mjvnu.eu-west-1.cs.amazonlightsail.com/')
+    #
+    # print(user_id)
+    # time.sleep(5)
