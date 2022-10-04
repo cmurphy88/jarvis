34f3298bca8b2b2233735fcad96dbc5e98cfcaea
cmurphy88 <cmurphy715@qub.ac.uk>
2022-10-04 11:15:13 +0100

    fixes

--- ui/src/shared/api/HomesAPI.jsx
+++ ui/src/shared/api/HomesAPI.jsx
@@ -3,15 +3,18 @@ import { axios } from "../../libs/axios";
 export async function getUsersHomes(id) {
     const response = await axios.get(`/homes/users/${id}`);
     return response.data;
-}
+};
 
 export async function addNewHome(name, user_id, is_admin) {
-    axios.post('/homes/create', {
+    const response = axios.post('/homes/create', {
         name: name,
         user_id: user_id,
         is_admin: is_admin
-    })
+    });
+
+    console.log(response.Promise);
+    return response;
     // axios.post(`/homes/${}`)
-}
+};
 
 
